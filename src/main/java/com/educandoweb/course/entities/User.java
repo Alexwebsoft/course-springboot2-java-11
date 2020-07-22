5c7eb911644242e750f5acd23eb216aa967e6e35
Alexwebsoft <alexwebsoft@gmail.com>
2020-07-22 01:40:51 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,24 +2,27 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-	
-	
-	
+@Entity
+public class User implements Serializable {
+
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	
-	public User() {
-		
-		
-	}
 
+	public User() {
+
+	}
 
 	public User(long id, String name, String email, String phone, String password) {
 		super();
@@ -30,56 +33,45 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-
 	public long getId() {
 		return id;
 	}
-
 
 	public void setId(long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhone() {
 		return phone;
 	}
-
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -88,7 +80,6 @@ public class User implements Serializable{
 		result = prime * result + (int) (id ^ (id >>> 32));
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -103,6 +94,5 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
